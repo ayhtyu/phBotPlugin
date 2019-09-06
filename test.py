d807ea2f910c25dcaa8a13cd1b8e918752a29320
ayhtyu <54457537+ayhtyu@users.noreply.github.com>
2019-09-06 20:48:32 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+import QtBind
+import urllib.request
+import re
+import os
+
+
+release = "https://github.com/ayhtyu/phBotPlugin/releases/latest"
+api = "https://api.github.com/repos/ayhtyu/phBotPlugin/releases/latest"
